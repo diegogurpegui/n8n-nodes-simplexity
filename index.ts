8975f7f908cd4f854d8cacac0e426f6c3304198d
Diego H. Gurpegui <contact@diegogurpegui.com>
2025-07-27 19:35:26 -0300

    Fix packaging

--- index.ts
+++
@@ -1,6 +0,0 @@
-import { SimpleXityTrigger } from './nodes/Simplexity/SimplexityTrigger.node';
-import { Simplexity } from './nodes/Simplexity/Simplexity.node';
-import { SimplexityApi } from './credentials/SimplexityApi.credentials';
-
-export const nodes = [SimpleXityTrigger, Simplexity];
-export const credentials = [SimplexityApi];
