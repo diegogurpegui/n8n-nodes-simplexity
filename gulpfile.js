4420a0d7d81afb06f6c4c709c01940cbdd026a1e
Diego H. Gurpegui <contact@diegogurpegui.com>
2025-07-27 20:06:14 -0300

    fix icons build

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
-const gulp = require('gulp');
+const path = require('path');
+const { task, src, dest } = require('gulp');
 
-gulp.task('build:icons', function() {
-	return gulp.src('nodes/**/simplexity.svg')
-		.pipe(gulp.dest('dist/nodes'));
-}); 
+task('build:icons', copyIcons);
+
+function copyIcons() {
+  const nodeSource = path.resolve('nodes', '**', '*.{png,svg}');
+  const nodeDestination = path.resolve('dist', 'nodes');
+
+  src(nodeSource).pipe(dest(nodeDestination));
+
+  const credSource = path.resolve('credentials', '**', '*.{png,svg}');
+  const credDestination = path.resolve('dist', 'credentials');
+
+  return src(credSource).pipe(dest(credDestination));
+}
